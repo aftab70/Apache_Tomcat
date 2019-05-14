6fb94eab8cf8cc2c25039a9059a561fb0cfe3d1f
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-05-14 23:47:48 +0530

    Update tomcat8_installation.sh

--- tomcat8_installation.sh
+++ tomcat8_installation.sh
@@ -19,7 +19,7 @@ sudo groupadd $SYSGROUP
 sudo useradd -s /bin/false -g $SYSUSER -d /opt/tomcat $SYSUSER
 
 sudo mkdir /opt/$TOMCATHOME
-sudo tar xzvf /opt/tapache-tomcat-8.5.38.tar.gz -C /opt/$TOMCATHOME --strip-components=1
+sudo tar xzvf /opt/apache-tomcat-8.5.38.tar.gz -C /opt/$TOMCATHOME --strip-components=1
 
 sudo chgrp -R $SYSUSER /opt/$TOMCATHOME
 sudo chmod -R g+r /opt/$TOMCATHOME/conf
@@ -29,6 +29,19 @@ sudo chown -R $SYSUSER /opt/$TOMCATHOME/webapps/
 sudo chown -R $SYSUSER /opt/$TOMCATHOME/work/
 sudo chown -R $SYSUSER /opt/$TOMCATHOME/temp/
 sudo chown -R $SYSUSER /opt/$TOMCATHOME/logs/
+
+#####################################################################################################################
+#AuthBind configuration in Tomcat port 80, 443
+
+sudo apt install authbind
+sudo touch /etc/authbind/byport/{443,80}
+sudo chmod 500 /etc/authbind/byport/{443,80}
+sudo chown tcat:tcat /etc/authbind/byport/{443,80}
+sudo sed -i 's/8080/80/g' /opt/tomcat/conf/server.xml
+sudo sed -i 's/8443/443/g' /opt/tomcat/conf/server.xml
+sudo echo "exec authbind --deep "$PRGDIR"/"$EXECUTABLE" start "$@"" >> /opt/tomcat/bin/startup.sh
+sudo systemctl restart tcat.service
+
 
 
 
