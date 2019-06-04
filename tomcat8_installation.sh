60781690ed2b7e806b737aaba0862e4c5878df31
aftab70 <43289912+aftab70@users.noreply.github.com>
2019-06-05 01:40:03 +0530

    Update tomcat8_installation.sh

--- tomcat8_installation.sh
+++ tomcat8_installation.sh
@@ -17,6 +17,7 @@ sudo chgrp -R tomcat /opt/tomcat
 sudo chmod -R g+r conf
 sudo chmod g+x conf
 sudo chown -R tomcat webapps/ work/ temp/ logs/
+
 sudo update-java-alternatives -l
 
 sudo nano /etc/systemd/system/tomcat.service
